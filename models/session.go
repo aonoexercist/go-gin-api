0177d37343f6e044595ec412f05e2405c932ded1
aonoexercist <xercisthegreat@gmail.com>
2026-03-30 14:37:40 +0800

    feat: add JSON tags to Session model fields for improved serialization

--- models/session.go
+++ models/session.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Session struct {
-	ID           uint
-	UserID       uint
-	RefreshToken string
-	UserAgent    string
-	IPAddress    string
-	ExpiresAt    time.Time
+	ID           uint      `gorm:"primaryKey" json:"id"`
+	UserID       uint      `json:"user_id"`
+	RefreshToken string    `json:"-"`
+	UserAgent    string    `json:"user_agent"`
+	IPAddress    string    `json:"ip_address"`
+	ExpiresAt    time.Time `json:"expires_at"`
 }
