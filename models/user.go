4d47bb404a2293a7e74e8abf9626dfb92ef21ca8
aonoexercist <xercisthegreat@gmail.com>
2026-03-31 16:37:04 +0800

    feat: add Google user authentication models and request structure

--- models/user.go
+++ models/user.go
@@ -1,8 +1,25 @@
 package models
 
 type User struct {
-	ID       uint   `gorm:"primaryKey" json:"id"`
-	Name     string `json:"name" binding:"required"`
-	Email    string `gorm:"unique" json:"email" binding:"required,email"`
-	Password string `json:"-" binding:"required"`
+	ID       uint    `gorm:"primaryKey" json:"id"`
+	Name     string  `json:"name" binding:"required"`
+	Email    string  `gorm:"unique" json:"email" binding:"required,email"`
+	Password string  `json:"-" binding:"required"`
+	GoogleID *string `gorm:"unique" json:"google_id,omitempty"`
+	Provider string  `json:"provider,omitempty"`
+}
+
+type GoogleUserInfo struct {
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Locale        string `json:"locale"`
+}
+
+type GoogleAuthRequest struct {
+	Token string `json:"token"`
 }
