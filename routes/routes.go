7fac3a1d7c16ffdc04053e23ece6abd96ecf541c
aonoexercist <xercisthegreat@gmail.com>
2026-04-02 14:08:35 +0800

    feat: add admin routes for user, role, and permission management

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,9 @@ package routes
 
 import (
 	"go-gin-api/controllers/auth"
+	"go-gin-api/controllers/rbac"
 	"go-gin-api/controllers/todo"
+	"go-gin-api/controllers/user"
 
 	"github.com/gin-gonic/gin"
 )
@@ -27,5 +29,31 @@ func SetupRoutes(r *gin.Engine) {
 		api.GET("/todos/:id", todo.GetTodo)
 		api.PUT("/todos/:id", todo.UpdateTodo)
 		api.DELETE("/todos/:id", todo.DeleteTodo)
+	}
+
+	adminApi := r.Group("/admin")
+	adminApi.Use(auth.AuthMiddleware(), rbac.RequireRole("super_admin"))
+	{
+		usersApi := adminApi.Group("/users")
+		{
+			usersApi.GET("/", user.GetUsers)
+			usersApi.GET("/:id", user.GetUser)
+			usersApi.PUT("/:id/roles", user.UpdateUserRoles)
+		}
+
+		rolesApi := adminApi.Group("/roles")
+		{
+			rolesApi.POST("/", rbac.CreateRole)
+			rolesApi.GET("/", rbac.GetRoles)
+			rolesApi.PUT("/:id", rbac.UpdateRole)
+			rolesApi.DELETE("/:id", rbac.DeleteRole)
+			rolesApi.PUT("/update/user", rbac.UpdateUserRolesHandler)
+		}
+
+		permissionsApi := adminApi.Group("/permissions")
+		{
+			permissionsApi.GET("/role/:id", rbac.GetPermissionsByRole)
+			permissionsApi.POST("/save", rbac.SaveRole)
+		}
 	}
 }
