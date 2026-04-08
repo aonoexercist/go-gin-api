1565ce742c73b46ec9e1ac2a2e63bef42f7a3410
aonoexercist <xercisthegreat@gmail.com>
2026-04-08 15:12:20 +0800

    feat: add CORS configuration and ping endpoint to main application

--- main.go
+++ main.go
@@ -5,12 +5,27 @@ import (
 	"go-gin-api/models"
 	"go-gin-api/routes"
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://xercisdev.theworkpc.com"}, // frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true, // IMPORTANT for cookies
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
 
 	config.ConnectDB()
 
